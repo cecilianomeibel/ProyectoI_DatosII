34e30a3e6434a62e8a61c18b45f917a2fcfee6bb
Meibel Ceciliano <cecilianomeibel@gmail.com>
2022-09-30 05:21:27 +0200

    Creacion de file Image Processing

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/gabrielwolf/Documents/WorkspaceProyectoI/ProyectoI_DatosII
+# Install script for directory: /home/meibel/Documentos/ProyectoI_DatosII
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/gabrielwolf/Documents/WorkspaceProyectoI/ProyectoI_DatosII/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/meibel/Documentos/ProyectoI_DatosII/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
